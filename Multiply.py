6a2692dcc0b39ccc8714456c8f9d5f7c1b2fd515
prudhvieng <41282493+prudhvieng@users.noreply.github.com>
2020-03-02 14:30:14 +0530

    Create Multiply.py

---
+++ Multiply.py
@@ -0,0 +1,7 @@
+num = int(input("enter the table number:"))
+len = int(input("enter the table length:"))
+def multiply(num):
+    for i in range(1,len+1):
+        type (range)
+        print("{} * {} = {}".format(num, i, num*i))
+multiply(num)
